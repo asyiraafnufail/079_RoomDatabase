ceef4035d1069bbede20712d9ac5e84aa682f47b
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-27 13:49:59 +0700

    import some model

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.repositori.RepositoriSiswa
 import com.example.roomdatabase.room.Siswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())

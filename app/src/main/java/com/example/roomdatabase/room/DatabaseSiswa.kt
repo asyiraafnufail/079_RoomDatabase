e44b9bb456214b5f2b5593fc7d52a7a05a099955
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-27 13:04:28 +0700

    feat(room): Implement singleton pattern for SiswaDatabase

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.roomdatabase.room
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class SiswaDatabase : RoomDatabase(){
+    abstract fun siswaDao(): SiswaDao
 
+    companion object{
+        @Volatile
+        private var INSTANCE: SiswaDatabase? = null
+
+        fun getDatabase(context: Context): SiswaDatabase{
+    }
 }

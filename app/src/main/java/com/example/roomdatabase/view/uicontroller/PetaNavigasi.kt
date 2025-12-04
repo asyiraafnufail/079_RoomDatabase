929a24cff28adc628c8a5127f80be63567307084
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-04 13:45:15 +0700

    remove comments

--- app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -10,10 +10,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.roomdatabase.view.DetailSiswaScreen
+import com.example.roomdatabase.view.EditSiswaScreen
 import com.example.roomdatabase.view.EntrySiswaScreen
 import com.example.roomdatabase.view.HomeScreen
 import com.example.roomdatabase.view.route.DestinasiDetailSiswa
 import com.example.roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
+import com.example.roomdatabase.view.route.DestinasiEditSiswa
 import com.example.roomdatabase.view.route.DestinasiHome
 import com.example.roomdatabase.view.route.DestinasiEntry
 

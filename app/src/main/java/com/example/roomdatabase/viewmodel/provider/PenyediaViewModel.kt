0ca6e27c4deed8cc6d012225dc8ff91c697c8991
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-04 12:01:04 +0700

    menambahkan viemodel baru

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
@@ -17,6 +18,11 @@ object PenyediaViewModel {
 
         initializer {
             EntryViewModel(aplikasiSiswa().containerApp.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().containerApp.repositoriSiswa
+            )
         }
     }
 }
